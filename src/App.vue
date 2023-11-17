950a16d7dbbd0c0e325f2f3887a3bff6e9afb005
Avisha <avishasodhi@yahoo.co.in>
2023-11-17 12:30:18 -0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <home-component />
+  <counter-component
+  :year = "2024"
+  :month = "10"
+  :date = "15"
+  :hour = "10"
+  :minute = "10"
+  :second = "10"
+  :millisecond = "10"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from './components/home-component.vue'
+import CounterComponent from './components/counter-component.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeComponent,
+    CounterComponent
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Times New Roman', Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #040f78;
+  background: -webkit-linear-gradient(right, rgb(161, 247, 230), rgb(9, 230, 199), rgb(20, 120, 106));
 }
 </style>
---
+++ src/components/home-component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>Hello! This is my first Vue.js app</h1>
+    <h1> Its solely for learning purpose and to have fun with Vue.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
